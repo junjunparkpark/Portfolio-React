b31aa3f619ddfefb93dda0e63d7244f35647b122
Jun Park <jun.park@live.com>
2017-10-30 16:48:20 -0700

    Set entry point and output for Webpack to compile React components in SRC client folder to Dist folder in bundle.js

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: './client/src/main.js',
+  output: {
+    path: path.join(__dirname, 'client/dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/,
+        include: path.join(__dirname, 'client/src'),
+        exclude: ['node_modules'],
+        use: [
+          { loader: 'babel-loader',
+            options: {
+              presets: ['react', 'es2015']
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'css-loader'
+        ]
+      }
+    ]
+  }
+};
+
+module.exports = config;
