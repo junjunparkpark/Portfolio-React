d2d56350307830542c7a28802d836ff5a991c770
Jun Park <jun.park@live.com>
2017-10-30 17:50:14 -0700

    Add style loaders, css loader and post-css loader in order to enable Bootstrap

--- client/src/Components/Navigation/Nav.jsx
+++ client/src/Components/Navigation/Nav.jsx
@@ -7,18 +7,20 @@ import AppBar from 'material-ui/AppBar';
  */
 const Nav = () => (
   <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
-    <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <a className="navbar-brand" href="#">Navbar</a>
-
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav mr-auto">
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
         <li className="nav-item active">
           <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
+          <a className="nav-link" href="#">Features</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Pricing</a>
         </li>
         <li className="nav-item">
           <a className="nav-link disabled" href="#">Disabled</a>
