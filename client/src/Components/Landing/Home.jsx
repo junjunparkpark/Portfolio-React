da1c70be31d5469be4e92f0ea35f188f1c96a2e2
Jun Park <jun.park@live.com>
2017-10-31 21:11:02 -0700

    No errors when React Home Component loads

--- client/src/Components/Landing/Home.jsx
+++ client/src/Components/Landing/Home.jsx
@@ -1,39 +1,44 @@
-
+import React from 'react';
 
 
 const Home = () => {
+  var tableStyle = { 
+    height: '100%', 
+    marginTop: '-70px'
+  };
+
   return (
     <div>
       <section id="preloader"></section>
-      <section id="home" class="text-center no-overflow gradient">
+      <section id="home" className="text-center no-overflow gradient">
 
-        <div class="container text-center">
-          <div class="fullscreen inline-block">
-            <div class="table" style="height: 100%; margin-top: -70px;">
-              <div class="table-cell v-middle">
+        <div className="container text-center">
+          <div className="fullscreen inline-block">
+            <div className="table" style={tableStyle}>
+              <div className="table-cell v-middle">
 
-                <div class="lg-rounded-box home__image-box">
-                    <img src="media/images/junpark.jpg" class="img-responsive" alt="#"/>
+                <div className="lg-rounded-box home__image-box">
+                    <img src="media/images/junpark.jpg" className="img-responsive" alt="#"/>
                 </div>
 
-                <h1 class="heavy text-uppercase home__name">jun park</h1>
+                <h1 className="heavy text-uppercase home__name">jun park</h1>
 
-                <span class="text-uppercase small home__profession">designer / developer / engineer</span>
-
-                  </div>
-
-                </div>
-
-            <div class="rings">
-                  <span>scroll</span>
-                  <div class="ring ring-1"></div>
-                  <div class="ring ring-2"></div>
-                  <div class="ring ring-3"></div>
-                </div>
+                <span className="text-uppercase small home__profession">designer / developer / engineer</span>
 
               </div>
 
             </div>
+
+            <div className="rings">
+              <span>scroll</span>
+              <div className="ring ring-1"></div>
+              <div className="ring ring-2"></div>
+              <div className="ring ring-3"></div>
+            </div>
+
+          </div>
+
+        </div>
       </section>
     </div>
   )
