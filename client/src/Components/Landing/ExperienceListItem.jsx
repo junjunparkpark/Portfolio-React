ab5af3888a8d5a75c118134dfdec0f757192728c
Jun Park <jun.park@live.com>
2017-11-01 13:50:12 -0700

    Add dummy data to ExperienceListItem component.

--- client/src/Components/Landing/ExperienceListItem.jsx
+++ client/src/Components/Landing/ExperienceListItem.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
 
 const ExperienceListItem = (props) => {
+  return (
 
+    {props.left 
+    ? 
+      <div className="experience">
+        <div className="experience-item">
+          <div className="icon-b wow bounceIn"><i className="fa fa-briefcase"></i></div>
+          <div className="experience-item-inner wow bounceInLeft">
+            <div className="date">Web Developer at OPSWAT</div>
+            <h3>October 2016 - Present</h3>
+            <p>OPSWAT is a San Francisco based software company that provides solutions to secure and manage IT infrastructure. I help develop and maintain the company's websites. <br /> Technologies Used - HTML, CSS, JavaScript, JQuery, Drupal, Craft, GhostInspector, Hubspot, Adobe Suite. </p>
+            <p>Latest Project - <a href="http://micro.opswat.com/" target="_blank">OPSWAT Microsite</a> A multi-lingual web platform.</p>
+          </div>
+        </div>
+    :
+      <div className="experience">
+        <div className="experience-item">
+          <div className="icon-b wow bounceIn"><i className="fa fa-briefcase"></i></div>
+          <div className="experience-item-inner wow bounceInRight">
+              <div className="date">Stanford University</div>
+              <h3>June - October, 2016</h3>
+              <p>Website management and integration of technology into Stanford's learning management system. </p>
+
+          </div>
+        </div>    
+    }
+
+  );
 };
 
 export default ExperienceListItem;
