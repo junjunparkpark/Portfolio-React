222fd576d2132b02b63abb01776b77f10bd3a6db
Jun Park <jun.park@live.com>
2017-10-31 23:21:26 -0700

    Create Experience component to show list of experiences/education

---
+++ client/src/Components/Landing/Experience.jsx
@@ -0,0 +1,107 @@
+import React from 'react';
+
+class ExperienceList extends React.Component {
+  constructor(props) {
+    super();
+
+  }
+
+
+
+  render() {
+    return (
+
+
+    <section id="expirience" class="section">
+                                                <div class="section-inner">
+
+                                                  <div class="container">
+
+                                                    <header class="heading">
+                                                      <h2>Experience</h2>
+                                                      <div class="separator"></div>
+                                                    </header>
+
+                                                    <div class="expirience">
+                                                      <div class="expirience-item">
+                                                        <div class="icon-b wow bounceIn"><i class="fa fa-briefcase"></i></div>
+                                                        <div class="expirience-item-inner wow bounceInLeft">
+                                                          <div class="date">Web Developer at OPSWAT</div>
+                                                          <h3>October 2016 - Present</h3>
+                                                          <p>OPSWAT is a San Francisco based software company that provides solutions to secure and manage IT infrastructure. I help develop and maintain the company's websites. <br> Technologies Used - HTML, CSS, JavaScript, JQuery, Drupal, Craft, GhostInspector, Hubspot, Adobe Suite. </p>
+                                                            <p>Latest Project - <a href="http://micro.opswat.com/" target="_blank">OPSWAT Microsite</a> A multi-lingual web platform.
+              </div>
+            </div>
+                                                          <div class="expirience-item">
+                                                            <div class="icon-b wow bounceIn"><i class="fa fa-briefcase"></i></div>
+                                                            <div class="expirience-item-inner wow bounceInLeft">
+                                                              <div class="date">LVRT, LLC</div>
+                                                              <p>LVRT (pronounced "Love Art") is a copyright licensing and promotional platform for musicians and filmmakers to distribute and fairly monetize their works.
+                <a href="http://www.lvrt.com/" target="_blank">LVRT</a>
+              </div>
+                                                            </div>
+                                                            <div class="expirience-item">
+                                                              <div class="icon-b wow bounceIn"><i class="fa fa-briefcase"></i></div>
+                                                              <div class="expirience-item-inner wow bounceInRight">
+                                                                <div class="date">Stanford University</div>
+                                                                <h3>June - October, 2016</h3>
+                                                                <p>Website management and integration of technology into Stanford's learning management system. </p>
+
+                                                              </div>
+                                                            </div>
+                                                            <div class="expirience-item">
+                                                              <div class="icon-b wow bounceIn"><i class="fa fa-briefcase"></i></div>
+                                                              <div class="expirience-item-inner wow bounceInRight">
+                                                                <div class="date">Part-time Mentor at Codify Academy</div>
+                                                                <h3>December 2016 - May, 2017</h3>
+                                                                <p>Collaborate with Instructors and actively engage with students to deliver a fully effective class in teaching Front End Development.</p>
+
+                                                              </div>
+                                                            </div>
+                                                            <div class="expirience-item">
+                                                              <div class="icon-b wow bounceIn"><i class="fa fa-briefcase"></i></div>
+                                                              <div class="expirience-item-inner wow bounceInLeft">
+                                                                <div class="date">Web developer at Design Science Inc.</div>
+                                                                <h3>2015</h3>
+                                                                <p>
+                                                                  Developed and Designed company's new website,
+			    Created responsive and intuitive UI
+			    </p>
+                                                              </div>
+                                                            </div>
+                                                            <div class="expirience-item">
+                                                              <div class="icon-b wow bounceIn"><i class="fa fa-mortar-board"></i></div>
+                                                              <div class="expirience-item-inner wow bounceInRight">
+                                                                <div class="date">California State University, Long Beach</div>
+                                                                <h3>2014 - 2016</h3>
+                                                                <p>Master of Science in Computer Science</p>
+                                                              </div>
+                                                            </div>
+                                                            <div class="expirience-item">
+                                                              <div class="icon-b wow bounceIn"><i class="fa fa-briefcase"></i></div>
+                                                              <div class="expirience-item-inner wow bounceInLeft">
+                                                                <div class="date">Freelance Web developer</div>
+                                                                <h3>2015 - 2016</h3>
+                                                                <p>Nomad Style & Pekok Within</p>
+                                                              </div>
+                                                            </div>
+                                                            <div class="expirience-item">
+                                                              <div class="icon-b wow bounceIn"><i class="fa fa-mortar-board"></i></div>
+                                                              <div class="expirience-item-inner wow bounceInRight">
+                                                                <div class="date">RGPV University</div>
+                                                                <h3>2010 - 2014</h3>
+                                                                <p>Bachelor of Engineering in Computer Science</p>
+                                                              </div>
+                                                            </div>
+                                                          </div>
+
+                                                        </div>
+
+                                                      </div>
+    </section>
+
+
+
+    )
+  }
+}
