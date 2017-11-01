4d6c37e7d2bb68b45a75728cfa6c720652c7e0e6
Jun Park <jun.park@live.com>
2017-10-31 21:03:23 -0700

    Remove Css loader in Webpack

--- client/src/main.js
+++ client/src/main.js
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Nav from './Components/Navigation/Nav.jsx';
 import Home from './Components/Landing/Home.jsx';
-import 'bootstrap';
-import 'jquery';
 
 const Main = () => {
   return (
     <div>
       <Home/>
-      <Nav />
     </div>
   );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -20,39 +20,8 @@ const config = {
           }
         ]
       },
-      {
-        test: /\.(scss)$/,
-        use: [{
-          loader: 'style-loader', // inject CSS to page
-        }, {
-          loader: 'css-loader', // translates CSS into CommonJS modules
-        }, {
-          loader: 'postcss-loader', // Run post css actions
-          options: {
-            plugins: function () { // post css plugins, can be exported to postcss.config.js
-              return [
-                require('precss'),
-                require('autoprefixer')
-              ];
-            }
-          }
-        }, {
-          loader: 'sass-loader' // compiles SASS to CSS
-        }]
-      },
     ]
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-      'window.jQuery': 'jquery',
-      Popper: ['popper.js', 'default'],
-      // In case you imported plugins individually, you must also require them here:
-      Util: "exports-loader?Util!bootstrap/js/dist/util",
-      Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown"
-    })
-  ]
+  }
 };
 
 module.exports = config;
