6935aed48692485a448f49cf8455646fac1afeb7
Jun Park <jun.park@live.com>
2017-11-01 13:28:45 -0700

    ApplicationListItems renders in a grid

--- client/src/Components/Landing/ApplicationsListItem.jsx
+++ client/src/Components/Landing/ApplicationsListItem.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const ApplicationsListItem = (props) => {
   return (
     <li className="works-item">
-      <a href="#works-item-details-5" className="works-item-link">
-        <span className="works-item-thumb"><img src="" width="250" height="250" alt="" /></span>
+      <a href="" className="works-item-link">
+        <span className="works-item-thumb"><img src="https://socialnewsdaily.com/wp-content/uploads/2014/05/rick-astley-rickrolling.jpg" width="250" height="250" alt="" /></span>
       </a>
-      <div id="works-item-details-5" className="popup mfp-hide">
+      <div id="" className="popup mfp-hide">
         <figure><img src="" width="750" height="500" alt="" /></figure>
         <div className="popup-inner">
           <h3>Chinese Zodiac</h3>
