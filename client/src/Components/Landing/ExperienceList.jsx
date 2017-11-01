da2093a1cbcb0c87fee284f026da226bfb2cdc04
Jun Park <jun.park@live.com>
2017-11-01 16:13:51 -0700

    Text Content in hover flipping div in Experiences is not centering still. Will refactor at a later date

--- client/src/Components/Landing/ExperienceList.jsx
+++ client/src/Components/Landing/ExperienceList.jsx
@@ -22,40 +22,29 @@ class ExperienceList extends React.Component {
           </header>
 
           <div className="experience">
-            <div className="experience-item">
-              <div className="icon-b wow bounceIn"><i className="fa fa-briefcase"></i></div>
-              <div className="experience-item-inner wow bounceInLeft">
-                <div className="date">Web Developer at OPSWAT</div>
-                <h3>October 2016 - Present</h3>
-                <p>OPSWAT is a San Francisco based software company that provides solutions to secure and manage IT infrastructure. I help develop and maintain the company's websites. <br/> Technologies Used - HTML, CSS, JavaScript, JQuery, Drupal, Craft, GhostInspector, Hubspot, Adobe Suite. </p>
-                  <p>Latest Project - <a href="http://micro.opswat.com/" target="_blank">OPSWAT Microsite</a> A multi-lingual web platform.</p>
-            </div>
-          </div>
-
-          <div className="experience-item">
-            <div className="icon-b wow bounceIn"><i className="fa fa-briefcase"></i></div>
-            <div className="experience-item-inner wow bounceInLeft">
-              <div className="date">LVRT, LLC</div>
-              <p>LVRT (pronounced "Love Art") is a copyright licensing and promotional platform for musicians and filmmakers to distribute and fairly monetize their works.
-              <a href="http://www.lvrt.com/" target="_blank">LVRT</a></p>
-            </div>
-          </div>
-
           <div className="experience-item">
             <div className="icon-b wow bounceIn"><i className="fa fa-briefcase"></i></div>
             <div className="experience-item-inner wow bounceInRight">
-              <div className="date">Stanford University</div>
-              <h3>June - October, 2016</h3>
-              <p>Website management and integration of technology into Stanford's learning management system. </p>
+                  <div className="date">Hacker-In-Residence (Software Engineering Fellowship)</div>
+              <h3>July 2017 - Present</h3>
+              <p>
+                Accommodated mentorship to engineering teams through code review, debugging, and architecture design.<br/>
+                Provided individualized instruction on topics including but not limited to computer science fundamentals, Javascript frameworks, and general back-end architecture.<br/>
+                Conducted mock software engineering technical interviews for students and alumni. 
+              </p>
             </div>
           </div>
 
           <div className="experience-item">
-            <div className="icon-b wow bounceIn"><i className="fa fa-briefcase"></i></div>
+            <div className="icon-b wow bounceIn"><i className="fa fa-mortar-board"></i></div>
             <div className="experience-item-inner wow bounceInRight">
-              <div className="date">Part-time Mentor at Codify Academy</div>
-              <h3>December 2016 - May, 2017</h3>
-              <p>Collaborate with Instructors and actively engage with students to deliver a fully effective className in teaching Front End Development.</p>
+              <div className="date">Hack Reactor</div>
+              <h3>May 2017 - July 2017</h3>
+              <p>
+                12 week software engineering program.<br/>
+                Focusing in computer science and Javascript fundamentals.<br/>
+                San Francisco Market - Cohort #78.
+              </p>
             </div>
           </div>
 
@@ -63,10 +52,10 @@ class ExperienceList extends React.Component {
             <div className="icon-b wow bounceIn"><i className="fa fa-mortar-board"></i></div>
             <div className="experience-item-inner wow bounceInLeft">
               <div className="date">Codify Academy</div>
-              <h3>Nov 2016 - Apr 2017</h3>
+              <h3>November 2016 - April 2017</h3>
               <p>
-                Web Development coding bootcamp in San Francisco, California.
-                Learned front end fundamentals: HTML, CSS, Javascript, JQuery, Bootstrap, Git, and AngularJS.
+              Web Development coding bootcamp in San Francisco, California.<br/>
+              Learned front end fundamentals: HTML, CSS, Javascript, JQuery, Bootstrap, Git, and AngularJS.
               </p>
             </div>
           </div>
@@ -75,11 +64,11 @@ class ExperienceList extends React.Component {
             <div className="icon-b wow bounceIn"><i className="fa fa-briefcase"></i></div>
             <div className="experience-item-inner wow bounceInRight">
               <div className="date">BitSigma, LLC</div>
-              <h3>Jun 2014 - Jan 2015</h3>
+              <h3>June 2014 - January 2015</h3>
               <p>
-                Bitcoin mining startup in Denver, Colorado.
-                Implemented a Bitcoin mining environment by converting office space into a datacenter.
-                Outputting ~25 Terahashes per second generating 1-2 Bitcoins per day at the time. 
+                Bitcoin mining startup in Denver, Colorado.<br/>
+                Implemented a Bitcoin mining environment by converting office space into a datacenter.<br/>
+                Outputted ~25 Terahashes per second generating 1-2 Bitcoins per day at the time. 
               </p>
             </div>
           </div>
@@ -87,9 +76,12 @@ class ExperienceList extends React.Component {
           <div className="experience-item">
             <div className="icon-b wow bounceIn"><i className="fa fa-briefcase"></i></div>
             <div className="experience-item-inner wow bounceInLeft">
-              <div className="date">Financial Representative</div>
-              <h3>Apr 2014 - Jun 2014</h3>
-              <p>Northwestern Mutual</p>
+              <div className="date">Northwestern Mutual</div>
+              <h3>April 2014 - June 2014</h3>
+              <p>
+                College Financial Representative.<br/>
+                Completed Northwestern Mutual's sales program for incoming financial representatives.
+              </p>
             </div>
           </div>
           
@@ -97,8 +89,11 @@ class ExperienceList extends React.Component {
             <div className="icon-b wow bounceIn"><i className="fa fa-mortar-board"></i></div>
             <div className="experience-item-inner wow bounceInRight">
               <div className="date">University of Denver</div>
-              <h3>2011 - 2015</h3>
-              <p>Bachelor of Science in Finance and Business Administration (Incomplete) </p>
+              <h3>August 2011 - November 2014</h3>
+              <p>
+                Bachelor of Science in Finance and Business Administration (Incomplete) <br/>
+                VP of Judicial Affairs and Risk Management - DU Greek Council
+              </p>
             </div>
           </div>
 
