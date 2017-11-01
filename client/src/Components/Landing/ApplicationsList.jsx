b4207ea516031b50caabfb2e1103ea0bd77efefa
Jun Park <jun.park@live.com>
2017-10-31 23:24:39 -0700

    Change class to className within render functions for JSX elements

--- client/src/Components/Landing/ApplicationsList.jsx
+++ client/src/Components/Landing/ApplicationsList.jsx
@@ -7,87 +7,87 @@ class ApplicationsList extends React.Component {
 
   render() {
     return (
-      <section id="projects" class="section">
-        <div class="section-inner">
+      <section id="projects" className="section">
+        <div className="section-inner">
 
-          <div class="container">
+          <div className="container">
 
-            <header class="heading">
+            <header className="heading">
               <h2>Projects</h2>
-              <div class="separator"></div>
+              <div className="separator"></div>
             </header>
 
-            <ul class="filter">
-              <li data-group="all" class="active">All</li>
+            <ul className="filter">
+              <li data-group="all" className="active">All</li>
               <li data-group="web">sites</li>
               <li data-group="illustration">apps</li>
             </ul>
 
-            <ul class="works">
-              <li data-groups="[&quot;web&quot;]" class="works-item"><a href="#works-item-details-1" class="works-item-link"><span class="works-item-thumb"><img src="images/lvrt1.png" width="250" height="250" alt=""></span></a>
-                <div id="works-item-details-1" class="popup mfp-hide">
+            <ul className="works">
+              <li data-groups="[&quot;web&quot;]" className="works-item"><a href="#works-item-details-1" className="works-item-link"><span className="works-item-thumb"><img src="images/lvrt1.png" width="250" height="250" alt=""></span></a>
+                <div id="works-item-details-1" className="popup mfp-hide">
                   <figure><img src="images/lvrt1.png" width="750" height="500" alt=""></figure>
-                    <div class="popup-inner">
+                    <div className="popup-inner">
                       <h3>Sunday Sessions by LVRT - Business Website</h3>
                       <p>A website for an LA based collective for LVRT llc.</p>
-                      <div class="center"><a href="https://ritagya.github.io/SundaySession/" target="_blank" class="btn">View site</a></div>
+                      <div className="center"><a href="https://ritagya.github.io/SundaySession/" target="_blank" className="btn">View site</a></div>
                     </div>
               </div>
             </li>
-                <li data-groups="[&quot;web&quot;]" class="works-item"><a href="#works-item-details-2" class="works-item-link"><span class="works-item-thumb"><img src="images/flat.png" width="250" height="250" alt=""></span></a>
-                  <div id="works-item-details-2" class="popup mfp-hide">
+                <li data-groups="[&quot;web&quot;]" className="works-item"><a href="#works-item-details-2" className="works-item-link"><span className="works-item-thumb"><img src="images/flat.png" width="250" height="250" alt=""></span></a>
+                  <div id="works-item-details-2" className="popup mfp-hide">
                     <figure><img src="images/flat.png" width="750" height="500" alt=""></figure>
-                      <div class="popup-inner">
+                      <div className="popup-inner">
                         <h3>Colorful Flat</h3>
                         <p>PSD template website.</p>
-                        <div class="center"><a href="https://ritagya.github.io/ColorfulFlat/" target="_blank" class="btn">View site</a></div>
+                        <div className="center"><a href="https://ritagya.github.io/ColorfulFlat/" target="_blank" className="btn">View site</a></div>
                       </div>
               </div>
             </li>
-                  <li data-groups="[&quot;web&quot;]" class="works-item works-item-t"><a href="#works-item-details-4" class="works-item-link"><span class="works-item-thumb"><img src="images/visual.png" width="250" height="250" alt=""></span></a>
-                    <div id="works-item-details-4" class="popup mfp-hide">
+                  <li data-groups="[&quot;web&quot;]" className="works-item works-item-t"><a href="#works-item-details-4" className="works-item-link"><span className="works-item-thumb"><img src="images/visual.png" width="250" height="250" alt=""></span></a>
+                    <div id="works-item-details-4" className="popup mfp-hide">
                       <figure><img src="images/visual.png" width="750" height="500" alt=""></figure>
-                        <div class="popup-inner">
+                        <div className="popup-inner">
                           <h3>Noah Andrews</h3>
                           <p>PSD conversion for Noah Andrews website | Personal website</p>
-                          <div class="center"><a href="https://ritagya.github.io/VisualArts/" target="_blank" class="btn">View site</a></div>
+                          <div className="center"><a href="https://ritagya.github.io/VisualArts/" target="_blank" className="btn">View site</a></div>
                         </div>
               </div>
             </li>
-                    <li data-groups="[&quot;illustration&quot;]" class="works-item"><a href="#works-item-details-7" class="works-item-link"><span class="works-item-thumb"><img src="images/money1.gif" width="250" height="250" alt=""></span></a>
-                      <div id="works-item-details-7" class="popup mfp-hide">
+                    <li data-groups="[&quot;illustration&quot;]" className="works-item"><a href="#works-item-details-7" className="works-item-link"><span className="works-item-thumb"><img src="images/money1.gif" width="250" height="250" alt=""></span></a>
+                      <div id="works-item-details-7" className="popup mfp-hide">
                         <figure><img src="images/budgetApp.png" width="750" height="500" alt=""></figure>
-                          <div class="popup-inner">
+                          <div className="popup-inner">
                             <h3>Budgety</h3>
                             <p>An interactive app to manage your monthly budget.</p>
-                            <div class="center"><a href="https://ritagya.github.io/Budgety/" target="_blank" class="btn">View site</a></div>
+                            <div className="center"><a href="https://ritagya.github.io/Budgety/" target="_blank" className="btn">View site</a></div>
                           </div>
               </div>
             </li>
-                      <li data-groups="[&quot;illustration&quot;]" class="works-item"><a href="#works-item-details-8" class="works-item-link"><span class="works-item-thumb"><img src="images/calendar1.gif" width="250" height="250" alt=""></span></a>
-                        <div id="works-item-details-8" class="popup mfp-hide">
+                      <li data-groups="[&quot;illustration&quot;]" className="works-item"><a href="#works-item-details-8" className="works-item-link"><span className="works-item-thumb"><img src="images/calendar1.gif" width="250" height="250" alt=""></span></a>
+                        <div id="works-item-details-8" className="popup mfp-hide">
                           <figure><img src="images/calendarly1.png" width="750" height="500" alt=""></figure>
-                            <div class="popup-inner">
+                            <div className="popup-inner">
                               <h3>Calendarly</h3>
                               <p>Application to help enterprises book appointments.</p>
-                              <div class="center"><a href="https://ritagya.github.io/Calendarly/" target="_blank" class="btn">View site</a></div>
+                              <div className="center"><a href="https://ritagya.github.io/Calendarly/" target="_blank" className="btn">View site</a></div>
                             </div>
               </div>
             </li>
-                        <li data-groups="[&quot;illustration&quot;]" class="works-item"><a href="#works-item-details-9" class="works-item-link"><span class="works-item-thumb"><img src="images/clock4.gif" width="250" height="250" alt=""></span></a>
-                          <div id="works-item-details-9" class="popup mfp-hide">
+                        <li data-groups="[&quot;illustration&quot;]" className="works-item"><a href="#works-item-details-9" className="works-item-link"><span className="works-item-thumb"><img src="images/clock4.gif" width="250" height="250" alt=""></span></a>
+                          <div id="works-item-details-9" className="popup mfp-hide">
                             <figure><img src="images/digitalClock.png" width="750" height="500" alt=""></figure>
-                              <div class="popup-inner">
+                              <div className="popup-inner">
                                 <h3>Digital Clock</h3>
                                 <p>A simple responsive digital clock made with <3 Javascript.</p>
-                                <div class="center"><a href="https://ritagya.github.io/Digital-Clock/" target="_blank" class="btn">View site</a></div>
+                                <div className="center"><a href="https://ritagya.github.io/Digital-Clock/" target="_blank" className="btn">View site</a></div>
                               </div>
               </div>
             </li>
-          <li data-groups="[&quot;illustration&quot;]" class="works-item works-item-w"><a href="#works-item-details-3" class="works-item-link"><span class="works-item-thumb"><img src="images/pig.png" width="250" height="250" alt=""></span></a>
-            <div id="works-item-details-3" class="popup mfp-hide">
+          <li data-groups="[&quot;illustration&quot;]" className="works-item works-item-w"><a href="#works-item-details-3" className="works-item-link"><span className="works-item-thumb"><img src="images/pig.png" width="250" height="250" alt=""></span></a>
+            <div id="works-item-details-3" className="popup mfp-hide">
               <figure><img src="images/pig-game.png" width="750" height="500" alt=""></figure>
-                <div class="popup-inner">
+                <div className="popup-inner">
                   <h3>The Pig Game</h3>
                   <p>GAME RULES:
             <ul style="text-align : left">
@@ -98,29 +98,29 @@ class ApplicationsList extends React.Component {
                       <li>The first player to reach 100 points on GLOBAL score wins the game</li>
                     </ul>
                   </p>
-                  <div class="center"><a href="https://ritagya.github.io/pig-game/" target="_blank" class="btn">View site</a></div>
+                  <div className="center"><a href="https://ritagya.github.io/pig-game/" target="_blank" className="btn">View site</a></div>
                 </div>
               </div>
 
             </li>
-          <li data-groups="[&quot;illustration&quot;]" class="works-item"><a href="#works-item-details-5" class="works-item-link"><span class="works-item-thumb"><img src="images/chinese.png" width="250" height="250" alt=""></span></a>
-            <div id="works-item-details-5" class="popup mfp-hide">
+          <li data-groups="[&quot;illustration&quot;]" className="works-item"><a href="#works-item-details-5" className="works-item-link"><span className="works-item-thumb"><img src="images/chinese.png" width="250" height="250" alt=""></span></a>
+            <div id="works-item-details-5" className="popup mfp-hide">
               <figure><img src="images/chinesehoro.png" width="750" height="500" alt=""></figure>
-                <div class="popup-inner">
+                <div className="popup-inner">
                   <h3>Chinese Zodiac</h3>
                   <p>An app created with Javascript where you can get a reading for your characteristics by entering your Chinese animal sign.</p>
-                  <div class="center"><a href="https://ritagya.github.io/Chinese_Horoscope_App/" target="_blank" class="btn">View site</a></div>
+                  <div className="center"><a href="https://ritagya.github.io/Chinese_Horoscope_App/" target="_blank" className="btn">View site</a></div>
                 </div>
               </div>
             </li>
 
-          <li data-groups="[&quot;illustration&quot;]" class="works-item"><a href="#works-item-details-6" class="works-item-link"><span class="works-item-thumb"><img src="images/shinchan1.png" width="250" height="250" alt=""></span></a>
-            <div id="works-item-details-6" class="popup mfp-hide">
+          <li data-groups="[&quot;illustration&quot;]" className="works-item"><a href="#works-item-details-6" className="works-item-link"><span className="works-item-thumb"><img src="images/shinchan1.png" width="250" height="250" alt=""></span></a>
+            <div id="works-item-details-6" className="popup mfp-hide">
               <figure><img src="images/shin.png" width="750" height="500" alt=""></figure>
-                <div class="popup-inner">
+                <div className="popup-inner">
                   <h3>Shin Chan Quiz</h3>
                   <p>A quiz app on a popular Japanese cartoon called Shin Chan.</p>
-                  <div class="center"><a href="https://ritagya.github.io/ShinChan_Quiz_App/" target="_blank" class="btn">View site</a></div>
+                  <div className="center"><a href="https://ritagya.github.io/ShinChan_Quiz_App/" target="_blank" className="btn">View site</a></div>
                 </div>
               </div>
             </li>
--- client/src/Components/Landing/Experience.jsx
+++ client/src/Components/Landing/Experience.jsx
@@ -12,56 +12,56 @@ class ExperienceList extends React.Component {
     return (
 
 
-    <section id="expirience" class="section">
-                                                <div class="section-inner">
+    <section id="expirience" className="section">
+                                                <div className="section-inner">
 
-                                                  <div class="container">
+                                                  <div className="container">
 
-                                                    <header class="heading">
+                                                    <header className="heading">
                                                       <h2>Experience</h2>
-                                                      <div class="separator"></div>
+                                                      <div className="separator"></div>
                                                     </header>
 
-                                                    <div class="expirience">
-                                                      <div class="expirience-item">
-                                                        <div class="icon-b wow bounceIn"><i class="fa fa-briefcase"></i></div>
-                                                        <div class="expirience-item-inner wow bounceInLeft">
-                                                          <div class="date">Web Developer at OPSWAT</div>
+                                                    <div className="expirience">
+                                                      <div className="expirience-item">
+                                                        <div className="icon-b wow bounceIn"><i className="fa fa-briefcase"></i></div>
+                                                        <div className="expirience-item-inner wow bounceInLeft">
+                                                          <div className="date">Web Developer at OPSWAT</div>
                                                           <h3>October 2016 - Present</h3>
                                                           <p>OPSWAT is a San Francisco based software company that provides solutions to secure and manage IT infrastructure. I help develop and maintain the company's websites. <br> Technologies Used - HTML, CSS, JavaScript, JQuery, Drupal, Craft, GhostInspector, Hubspot, Adobe Suite. </p>
                                                             <p>Latest Project - <a href="http://micro.opswat.com/" target="_blank">OPSWAT Microsite</a> A multi-lingual web platform.
               </div>
             </div>
-                                                          <div class="expirience-item">
-                                                            <div class="icon-b wow bounceIn"><i class="fa fa-briefcase"></i></div>
-                                                            <div class="expirience-item-inner wow bounceInLeft">
-                                                              <div class="date">LVRT, LLC</div>
+                                                          <div className="expirience-item">
+                                                            <div className="icon-b wow bounceIn"><i className="fa fa-briefcase"></i></div>
+                                                            <div className="expirience-item-inner wow bounceInLeft">
+                                                              <div className="date">LVRT, LLC</div>
                                                               <p>LVRT (pronounced "Love Art") is a copyright licensing and promotional platform for musicians and filmmakers to distribute and fairly monetize their works.
                 <a href="http://www.lvrt.com/" target="_blank">LVRT</a>
               </div>
                                                             </div>
-                                                            <div class="expirience-item">
-                                                              <div class="icon-b wow bounceIn"><i class="fa fa-briefcase"></i></div>
-                                                              <div class="expirience-item-inner wow bounceInRight">
-                                                                <div class="date">Stanford University</div>
+                                                            <div className="expirience-item">
+                                                              <div className="icon-b wow bounceIn"><i className="fa fa-briefcase"></i></div>
+                                                              <div className="expirience-item-inner wow bounceInRight">
+                                                                <div className="date">Stanford University</div>
                                                                 <h3>June - October, 2016</h3>
                                                                 <p>Website management and integration of technology into Stanford's learning management system. </p>
 
                                                               </div>
                                                             </div>
-                                                            <div class="expirience-item">
-                                                              <div class="icon-b wow bounceIn"><i class="fa fa-briefcase"></i></div>
-                                                              <div class="expirience-item-inner wow bounceInRight">
-                                                                <div class="date">Part-time Mentor at Codify Academy</div>
+                                                            <div className="expirience-item">
+                                                              <div className="icon-b wow bounceIn"><i className="fa fa-briefcase"></i></div>
+                                                              <div className="expirience-item-inner wow bounceInRight">
+                                                                <div className="date">Part-time Mentor at Codify Academy</div>
                                                                 <h3>December 2016 - May, 2017</h3>
-                                                                <p>Collaborate with Instructors and actively engage with students to deliver a fully effective class in teaching Front End Development.</p>
+                                                                <p>Collaborate with Instructors and actively engage with students to deliver a fully effective className in teaching Front End Development.</p>
 
                                                               </div>
                                                             </div>
-                                                            <div class="expirience-item">
-                                                              <div class="icon-b wow bounceIn"><i class="fa fa-briefcase"></i></div>
-                                                              <div class="expirience-item-inner wow bounceInLeft">
-                                                                <div class="date">Web developer at Design Science Inc.</div>
+                                                            <div className="expirience-item">
+                                                              <div className="icon-b wow bounceIn"><i className="fa fa-briefcase"></i></div>
+                                                              <div className="expirience-item-inner wow bounceInLeft">
+                                                                <div className="date">Web developer at Design Science Inc.</div>
                                                                 <h3>2015</h3>
                                                                 <p>
                                                                   Developed and Designed company's new website,
@@ -69,26 +69,26 @@ class ExperienceList extends React.Component {
 			    </p>
                                                               </div>
                                                             </div>
-                                                            <div class="expirience-item">
-                                                              <div class="icon-b wow bounceIn"><i class="fa fa-mortar-board"></i></div>
-                                                              <div class="expirience-item-inner wow bounceInRight">
-                                                                <div class="date">California State University, Long Beach</div>
+                                                            <div className="expirience-item">
+                                                              <div className="icon-b wow bounceIn"><i className="fa fa-mortar-board"></i></div>
+                                                              <div className="expirience-item-inner wow bounceInRight">
+                                                                <div className="date">California State University, Long Beach</div>
                                                                 <h3>2014 - 2016</h3>
                                                                 <p>Master of Science in Computer Science</p>
                                                               </div>
                                                             </div>
-                                                            <div class="expirience-item">
-                                                              <div class="icon-b wow bounceIn"><i class="fa fa-briefcase"></i></div>
-                                                              <div class="expirience-item-inner wow bounceInLeft">
-                                                                <div class="date">Freelance Web developer</div>
+                                                            <div className="expirience-item">
+                                                              <div className="icon-b wow bounceIn"><i className="fa fa-briefcase"></i></div>
+                                                              <div className="expirience-item-inner wow bounceInLeft">
+                                                                <div className="date">Freelance Web developer</div>
                                                                 <h3>2015 - 2016</h3>
                                                                 <p>Nomad Style & Pekok Within</p>
                                                               </div>
                                                             </div>
-                                                            <div class="expirience-item">
-                                                              <div class="icon-b wow bounceIn"><i class="fa fa-mortar-board"></i></div>
-                                                              <div class="expirience-item-inner wow bounceInRight">
-                                                                <div class="date">RGPV University</div>
+                                                            <div className="expirience-item">
+                                                              <div className="icon-b wow bounceIn"><i className="fa fa-mortar-board"></i></div>
+                                                              <div className="expirience-item-inner wow bounceInRight">
+                                                                <div className="date">RGPV University</div>
                                                                 <h3>2010 - 2014</h3>
                                                                 <p>Bachelor of Engineering in Computer Science</p>
                                                               </div>
