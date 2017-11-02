e35be64417bbfdd89c0b4741e2d53a5190960c75
Jun Park <jun.park@live.com>
2017-11-01 19:35:50 -0700

    Add more skills pictures and implement final cut of skills

--- client/src/Components/Landing/Skills.jsx
+++ client/src/Components/Landing/Skills.jsx
@@ -16,42 +16,53 @@ const Skills = (props) => {
           <div class="tools">
             <div class="icon-b new">
               <div class="icons"><img src="media/images/html.png" alt="HTML5" title="HTML5" width="40" height="40"/></div>
-                <h3>HTML5</h3>
-              </div>
-              <div class="icon-b new">
-                <div class="icons"><img src="media/images/css.png" alt="CSS3" title="CSS3" width="40" height="40"/></div>
-                  <h3>CSS3</h3>
-                </div>
-                <div class="icon-b new">
-                  <div class="icons"><img src="media/images/js.png" alt="Javascript" title="Javascript" width="40" height="40"/></div>
-                    <h3>Javascript</h3>
-                  </div>
-                  <div class="icon-b new">
-                    <div class="icons"><img src="media/images/jq.png" alt="Jquery" title="Jquery" width="40" height="40"/></div>
-                      <h3>Jquery</h3>
+              <h3>HTML</h3>
+            </div>
+              
+            <div class="icon-b new">
+              <div class="icons"><img src="media/images/css.png" alt="CSS3" title="CSS3" width="40" height="40"/></div>
+              <h3>CSS</h3>
+            </div>
 
-                    </div>
-                    <div class="icon-b new">
-                      <div class="icons"><img src="media/images/b.png" alt="Bootstrap" title="Bootstrap" width="40" height="40"/></div>
-                        <h3>Bootstrap</h3>
-                      </div>
-                      <div class="icon-b new">
-                        <div class="icons"><img src="media/images/react.png" alt="react" title="react" width="30" height="30"/></div>
-                          <h3>React</h3>
-                        </div>
+            <div class="icon-b new">
+              <div class="icons"><img src="media/images/js.png" alt="Javascript" title="Javascript" width="30" height="30"/></div>
+              <h3>Javascript</h3>
+            </div>
 
-                        <div class="icon-b new">
-                          <div class="icons"><img src="media/images/json.png" alt="Json" title="Json" width="50" height="50"/></div>
-                            <h3>Json</h3>
-                          </div>
-                          <div class="icon-b new">
-                            <div class="icons"><img src="media/images/ajax.png" alt="Ajax" title="ajax" width="50" height="50"/></div>
-                              <h3>AJAX</h3>
-                            </div>
-                          </div>
-                        </div>
+            <div class="icon-b new">
+              <div class="icons"><img src="media/images/react.png" alt="react" title="react" width="30" height="30" /></div>
+              <h3>React</h3>
+            </div>
 
-                      </div>
+            <div class="icon-b new">
+              <div class="icons"><img src="media/images/angular.png" alt="angular" title="angular" width="40" height="40"/></div>
+              <h3>Angular</h3>
+            </div>
+
+            <div class="icon-b new">
+              <div class="icons"><img src="media/images/backbone.png" alt="Backbone" title="Backbone" width="25" height="25"/></div>
+              <h3>Backbone</h3>
+            </div>
+                  
+
+            <div class="icon-b new">
+              <div class="icons"><img src="media/images/node.png" alt="node" title="node" width="35" height="35"/></div>
+              <h3>Node</h3>
+            </div>
+
+            <div class="icon-b new">
+              <div class="icons"><img src="media/images/sql.png" alt="sql" title="sql" width="40" height="40"/></div>
+              <h3>SQL</h3>
+            </div>
+
+            <div class="icon-b new">
+              <div class="icons"><img src="media/images/mongo.png" alt="mongo" title="mongo" width="40" height="40" /></div>
+              <h3>Mongo</h3>
+            </div>
+          </div>
+        </div>
+
+      </div>
     </section>
   );
 };
